7bd90f9b13c344e2c0d7628463560c7471d05403
unknown <ranaivoharitiananomena@gmail.com>
2020-12-18 07:39:55 +0300

    finalising project #master

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,6 +1,6 @@
-let toggleBtn = document.querySelector(".menu-toggle");
-let sidebar = document.querySelector(".sidebar");
+let toggleBtn = document.querySelector('.menu-toggle');
+let sidebar = document.querySelector('.sidebar');
 
 toggleBtn.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
+  sidebar.classList.toggle('open');
 });
